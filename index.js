bfa86c3baee15d6396cc6b09556002a92af72615
decoded-cipher <arjun12345krishna@gmail.com>
2020-02-22 05:56:44 +0530

    C:/Program Files/Git/bookmarkURL command

--- index.js
+++ index.js
@@ -1,79 +1,75 @@
 process.env.NTBA_FIX_319 = 1;
 
 const TelegramBot = require('node-telegram-bot-api');
+const ogs = require('open-graph-scraper');
+const firebase = require('firebase');
 
-// replace the value below with the Telegram token you receive from @BotFather
+// Bot config
 const token = '1060187211:AAG4N-2oGbyLQRf-VPPJ2sOIXhOKRyyv25M';
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const firebaseConfig = {
+// Init Firebase
+const app = firebase.initializeApp({
     apiKey: "AIzaSyB28S5IBW23PntFWynQqB33ZzGkzQnf9mI",
     authDomain: "my-cipher-bot.firebaseapp.com",
-    databaseURL: "https://my-cipher-bot.firebaseio.com/",
+    databaseURL: "https://my-cipher-bot.firebaseio.com",
     projectId: "my-cipher-bot",
     storageBucket: "my-cipher-bot.appspot.com",
     messagingSenderId: "160844162176",
     appId: "1:160844162176:web:22f27f0f24d71bf50bb265",
     measurementId: "G-R7KLMTPJ1S"
-  };
-
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message');
-// });
-
-const firebase = require('firebase');
-firebase.initializeApp(firebaseConfig);
+});
 
 const ref = firebase.database().ref();
 const sitesRef = ref.child("sites");
 
 let siteUrl;
+
+// Reply to /bookmark
 bot.onText(/\/bookmark (.+)/, (msg, match) => {
   siteUrl = match[1];
-  bot.sendMessage(msg.chat.id,'Got it Arjun. Which category does it belong to...?', {
+  bot.sendMessage(msg.chat.id,'Got it, in which category?', {
     reply_markup: {
       inline_keyboard: [[
         {
-          text: 'HTML',
-          callback_data: 'HTML'
+          text: 'Development',
+          callback_data: 'development'
         },{
-          text: 'CSS',
-          callback_data: 'CSS'
+          text: 'Music',
+          callback_data: 'music'
         },{
-          text: 'JavaScript',
-          callback_data: 'JavaScript'
-        },{
-            text: 'Others',
-            callback_data: 'Others'
-          }
+          text: 'Cute monkeys',
+          callback_data: 'cute-monkeys'
+        }
       ]]
     }
   });
 });
 
+// Callback query
 bot.on("callback_query", (callbackQuery) => {
-    const message = callbackQuery.message;
-    ogs({'url': siteUrl}, function (error, results) {
-      if(results.success) {
-        sitesRef.push().set({
-          name: results.data.ogSiteName,
-          title: results.data.ogTitle,
-          description: results.data.ogDescription,
-          url: siteUrl,
-          thumbnail: results.data.ogImage.url,
-          category: callbackQuery.data
-        });
-        bot.sendMessage(message.chat.id,'Added \"' + results.data.ogTitle +'\" to category \"' + callbackQuery.data + '\"!')
-  } else {
-        sitesRef.push().set({
-          url: siteUrl
-        });
-        bot.sendMessage(message.chat.id,'Added new website, but there was no OG data!');
-      }
-    });
+  const message = callbackQuery.message;
+  // Scrap OG date
+  ogs({'url': siteUrl}, function (error, results) {
+    if(results.success) {
+      // Push to Firebase
+      sitesRef.push().set({
+        name: results.data.ogSiteName,
+        title: results.data.ogTitle,
+        description: results.data.ogDescription,
+        url: siteUrl,
+        thumbnail: results.data.ogImage.url,
+        category: callbackQuery.data
+      });
+      // Reply 
+      bot.sendMessage(message.chat.id,'Added \"' + results.data.ogTitle +'\" to category \"' + callbackQuery.data + '\"!');
+    } else {
+      // Push to Firebase
+      sitesRef.push().set({
+        url: siteUrl
+      });
+      // Reply 
+      bot.sendMessage(message.chat.id,'Added new website, but there was no OG data!');
+    }
   });
+});
